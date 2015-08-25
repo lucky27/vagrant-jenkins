8ca47d2492fc4902dadcb9a8e0ea5fea50348398
Ben Snape <ben.snape@itv.com>
2015-08-25 10:02:55 +0100

    added workflow plugins and required dependencies

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,4 +1,26 @@
 node default {
   include jenkins
   include git
+
+  jenkins::plugin {
+    "greenballs" : ;
+    'credentials' : ;
+    'ssh-credentials' : ;
+    'durable-task' : ;
+    'git-server' : ;
+    'git-client' : ;
+    'scm-api' : ;
+    'git' : ;
+    'workflow-aggregator' : ;
+    'workflow-job' : ;
+    'workflow-step-api' : ;
+    'workflow-cps' : ;
+    'workflow-support' : ;
+    'workflow-durable-task-step' : ;
+    'workflow-basic-steps' : ;
+    'workflow-scm-step' : ;
+    'workflow-api' : ;
+    'workflow-cps-global-lib' : ;
+  }
+
 }
